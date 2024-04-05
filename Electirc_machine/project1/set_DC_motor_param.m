7bc2b7aabfa673803d049733a0fbf9f5ac5fffb4
jinooLi-lab <jinu1423@naver.com>
2024-04-05 16:47:24 +0900

    add set_DC_motor_param.m

---
+++ Electirc_machine/project1/set_DC_motor_param.m
@@ -0,0 +1,14 @@
+% DC motor Parameters
+Power = 3336;               % power[w]
+Va_rated = 140;             % Rated Voltage [V]
+Ia_rated = 25;              % Rated Current [I]
+Wm_rated = 3000*2*pi/60;    % Rated Angular Velocity[rad/s]
+Te_rated = Power/Wm_rated;  % Rated Torque [Nm]
+
+
+Ra = 0.26;                  % Amature Resistance [Ohm]
+La = 1.7e-3;                % Amature Inductance [H]
+J = .00252;                 % Moment of Inertia [kg-m^2]
+B = 0.1;                    % Coefficient of Viscous Friction [kgm^2/s]
+
+K = Te_rated/Ia_rated;      % Kt+Flux
