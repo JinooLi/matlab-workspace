82b38d40206c0900b35fa9fad61a4675d65ff549
jinoo <jinu1423@naver.com>
2024-04-12 22:06:49 +0900

    문제 4번 풂

---
+++ Electirc_machine/project1/p4.m
@@ -0,0 +1,31 @@
+%% 문제 4
+% clean
+clc; clear all; close all;
+
+% set param
+set_DC_motor_param
+
+% run simulink
+out = sim('motor.slx');
+
+% plot
+nexttile
+plot(out.tout,out.chopOut.Data(:,1),LineWidth=2,Color=[1 0 0]);
+hold on
+plot(out.tout,out.chopOut.Data(:,2),LineWidth=2,Color=[0 0 1]);
+xlabel('Time[sec]')
+ylabel('velocity[r/min]')
+xlim([0 0.2])
+legend('Wrpm_{ref}','Wrpm')
+grid on
+
+nexttile
+plot(out.tout,out.chopOut.Data(:,3),LineWidth=2,Color=[1 0 0]);
+hold on
+plot(out.tout,out.chopOut.Data(:,4),LineWidth=2,Color=[0 0 1]);
+xlabel('Time[sec]')
+ylabel('')
+xlim([0 0.2])
+legend('Ia [A]','Te [Nm]')
+grid on
+
