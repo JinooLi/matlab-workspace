424a5ddb6595497753a004d0161fa3a2c28875c8
jinooLi-lab <jinu1423@naver.com>
2024-04-16 21:33:47 +0900

    찐찐막

--- Electirc_machine/project1/set_DC_motor_param.m
+++ Electirc_machine/project1/set_DC_motor_param.m
@@ -5,22 +5,21 @@ Ia_rated = 25;              % Rated Current [I]
 Wm_rated = 3000*2*pi/60;    % Rated Angular Velocity[rad/s]
 Te_rated = Power/Wm_rated;  % Rated Torque [Nm]
 
-
 Ra = 0.26;                  % Amature Resistance [Ohm]
 La = 1.7e-3;                % Amature Inductance [H]
 J = .00252;                 % Moment of Inertia [kgm^2]
-B = 0.0;                    % Coefficient of Viscous Friction [kgm^2/s]
+B = 0.01;                   % Coefficient of Viscous Friction [kgm^2/s]
 
 K = Te_rated/Ia_rated;      % Kt*Flux
 
 % Current Controller Parameters
-Wcc = 10000;                 % 대역폭 [Hz]
+Wcc = 1000;                 % 대역폭 [Hz]
 Kpc = La*Wcc;               % P-gain
 Kic = Ra*Wcc;               % I-gain
 Kac = 1/Kpc;                % Anti-Windup gian
 
 % velocity Controller Parameters
-Wcs = 1000;                  
+Wcs = 100;                  
 Kps = J*Wcs/K;
 Kis = J*Wcs*Wcs/(5*K);
 Kas = 1/Kps;
