cd2dd3f7c70835a81d8b6263cdac6a7fbe677379
jinooLi-lab <jinu1423@naver.com>
2024-06-12 14:23:47 +0900

    Hw2 수정

--- Digital_Control/HW_2/hw2_3_d.m
+++ Digital_Control/HW_2/hw2_3_d.m
@@ -2,6 +2,14 @@ T = 0.2;
 num = [1 0 0 0.5];
 den = [1 0 -0.4 0 0];
 F_tf = tf(num,den,T);
-K = 0.5;
+K = 0.9;
 
+% 단위원 생성
+theta = linspace(0,2*pi(),100);
+x = sin(theta);
+y = cos(theta);
+
+plot(x,y,'red')
+hold on
 nyquist(K*F_tf)
+hold off
