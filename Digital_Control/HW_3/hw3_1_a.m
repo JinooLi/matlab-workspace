e49c90d8c867e9491ea6bd038c5cb590018a4e13
jinooLi-lab <jinu1423@naver.com>
2024-06-12 17:03:00 +0900

    HW3중

--- Digital_Control/HW_3/hw3_1_a.m
+++ Digital_Control/HW_3/hw3_1_a.m
@@ -1,45 +1,37 @@
-clc; clear all; close all;  
+clc; clear; close all;
 % 모델 생성
 num = [5 20];
 den = [1 0.5 4];
 Gp_tf = tf(num,den);
-
 T=0.2;
 Gd_tf = c2d(Gp_tf,T,'zoh');
 
-% 제어기 생성
-% wp =< w0
-w1 = 
-w0 = 5;
-wp = 0.1*w0;
-Kc = 0.1;
+margin(Gd_tf);
 
-% z = e^{sT}
-z0 = exp(w0*T);
-zp = exp(wp*T);
+Gw_tf = d2c(Gd_tf,'tustin');
+figure();
+margin(Gw_tf); grid on;
 
-num = [1 z0];
-den = [1 zp];
-Cd_lag_tf = Kc*tf(num,den,T);
+om_w1 = 2.49;
 
-% open loop transfer function 생성
-openloop_tf = Cd_lag_tf*Gd_tf;
+k1to0 = 0.005;% 0.1 이하로 설정.
 
-%bode diagram, root locus, nyquist plot 출력
-subplot(2,2,1);
-margin(Gd_tf);hold on;
-margin(openloop_tf);grid on;
+om_w0 = k1to0*om_w1;
+
+a0 = 1.2; % 임의로 설정
+
+k_wp = 0.6; % 1이하로 설정
+
+om_wp = k_wp*om_w0/(a0*exp(20.1/20));
+
+Dw_lag_tf = a0*tf([1/om_w0 1], [1/om_wp 1]);
+figure();
+bode(Dw_lag_tf); grid on;
+
+figure();
+margin(Gw_tf); hold on;
+margin(Gw_tf * Dw_lag_tf); grid on;
 legend('Without compensator', 'With compensator', 'Interpreter', ' latex');
 
-subplot(2,2,2);
-nyquist(openloop_tf);
-
-subplot(2,2,3);
-rlocus(openloop_tf)
-
-subplot(2,2,4);
-step(feedback(openloop_tf,1))
 
 
-% 제어기 출력
-Cd_lag_tf
