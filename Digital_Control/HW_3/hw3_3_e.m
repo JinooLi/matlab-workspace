d6d6b24dbd38eccef4ed96186b04ac1477a9cba8
jinooLi-lab <jinu1423@naver.com>
2024-06-13 13:53:36 +0900

    찐막

--- Digital_Control/HW_3/hw3_3_e.m
+++ Digital_Control/HW_3/hw3_3_e.m
@@ -14,7 +14,8 @@ Gd_ss = ss(Gd_tf);
 % igenvalue를 p 벡터로 설정하는 벡터 L생성
 p = [0.2 0.5];
 K = place(Gd_ss.A, Gd_ss.B, p);
-L = place(Gd_ss.A', Gd_ss.C', p)';
+p = [-0.1 0.2211];
+L = place((Gd_ss.A - Gd_ss.B*K)', Gd_ss.C', p)';
 
 % 초기 상태 벡터 X0 설정
 X0 = [1; 1];
--- Digital_Control/HW_3/hw3_3_f.m
+++ Digital_Control/HW_3/hw3_3_f.m
@@ -11,10 +11,12 @@ Gd_tf = c2d(Gp_tf,T,'zoh');
 % 상태 변수 모델로 변환
 Gd_ss = ss(Gd_tf);
 
-% igenvalue를 p 벡터로 설정하는 벡터 L생성
-p = [0.2 0.5];
+% igenvalue를 p 벡터로 설정하는 벡터 L, K생성
+p = [-0.1 0.2211];
 K = place(Gd_ss.A, Gd_ss.B, p);
-L = place(Gd_ss.A', Gd_ss.C', p)';
+L = place((Gd_ss.A - Gd_ss.B*K)', Gd_ss.C', p)';
+%L = place(Gd_ss.A', Gd_ss.C', p)';
+
 
 % 전체 전달함수 생성
 An = Gd_ss.A - Gd_ss.B*K - L*Gd_ss.C;
@@ -22,3 +24,8 @@ Bn = L;
 Cn = K;
 tfn = tf(ss(An,Bn,Cn,0,T));
 tfn
+
+% 시뮬레이션
+z = tf('z',T);
+figure;
+step(tfn); grid on;
