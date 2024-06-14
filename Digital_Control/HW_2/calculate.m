096856f7d179fc4555a99582017a31365e7beef6
jinooLi-lab <jinu1423@naver.com>
2024-06-14 22:33:36 +0900

    Hw2 1.c. 계산 고침

--- Digital_Control/HW_2/calculate.m
+++ Digital_Control/HW_2/calculate.m
@@ -4,26 +4,70 @@ syms t;
 % Define the function
 b = -(0.8+t)+(t+1)*exp(-0.2/t);
 c = t + (0.8-t)*exp(-0.2/t);
-p = -b/2+sqrt(c);
-q = -b/2-sqrt(c);
 
-% drwa the function
+% function for jury stability criterion
+s1 = b+c+1;
+s2 = -b+c+1;
+s3 = 1-abs(c);
+
+% drwa function s1
 figure;
 range = [-10,-0.1];
-fplot(p, range); hold on;
-fplot(q, range); hold on;
-fplot(1, range); hold on;
-fplot(-1, range); hold on;
+subplot(1,2,1);
+fplot(s1, range);
 
-figure;
 range = [0,10];
-fplot(p, range); hold on;
-fplot(q, range); hold on;
-fplot(1, range); hold on;
-fplot(-1, range); hold on;
+subplot(1,2,2);
+fplot(s1, range);
 
-% finde limit of the function as t approches to infinity
-limit(p, t, inf)
-limit(q, t, inf)
-limit(p, t, -inf)
-limit(q, t, -inf)
+% Calculate the limit
+s1r = limit(s1, t, 0, 'right');
+s1l = limit(s1, t, 0, 'left');
+
+% print limit
+disp('s1r = ');
+disp(s1r);
+disp('s1l = ');
+disp(s1l);
+
+% drwa function s2
+figure;
+range = [-10,-0.1];
+subplot(1,2,1);
+fplot(s2, range);
+
+range = [0,10];
+subplot(1,2,2);
+fplot(s2, range);
+
+% Calculate the limit
+s2r = limit(s2, t, 0, 'right');
+s2l = limit(s2, t, 0, 'left');
+
+% print limit
+disp('s2r = ');
+disp(s2r);
+disp('s2l = ');
+disp(s2l);
+
+% drwa function s3
+figure;
+range = [-10,-0.1];
+subplot(1,2,1);
+fplot(s3, range);
+
+range = [0,10];
+subplot(1,2,2);
+fplot(s3, range);
+
+% Calculate the limit
+s3r = limit(s3, t, 0, 'right');
+s3l = limit(s3, t, 0, 'left');
+
+% print limit
+disp('s3r = ');
+disp(s3r);
+disp('s3l = ');
+disp(s3l);
+
+
